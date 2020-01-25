7f59cd21875457c414050067b2879d43c5f441d0
sonali <sonalipatil25oct@gmail.com>
2020-01-25 14:01:50 +0530

    initial commit

---
+++ demo.rb
@@ -0,0 +1,17 @@
+class Student
+  def initialize(name,age)
+    @name = name
+    @age = age
+  end
+
+  def intro
+    p "My name is {@name}"
+    p "My age is #{@age}"
+  end
+end
+p "Enter your name"
+name = gets.chomp
+p "Enter your age"
+age = gets.chomp
+s = Student.new(name, age)
+s.intro
